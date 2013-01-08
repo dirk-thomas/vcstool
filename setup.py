d637b738b7c00dc0b9eaa2ffad73cdf575b554db
Dirk Thomas <dthomas@willowgarage.com>
2013-01-08 10:16:26 -0800

    update meta information in setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ from setuptools import setup, find_packages
 setup(
     name='vcstool',
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     author='Dirk Thomas',
     author_email='dthomas@willowgarage.com',
     maintainer='Dirk Thomas',
     maintainer_email='dthomas@willowgarage.com',
     url='http://www.ros.org/wiki/vcstool',
     download_url='http://pr.willowgarage.com/downloads/vcstool/',
-    keywords=['VCS'],
-    classifiers=['Programming Language :: Python',
-                 'License :: OSI Approved :: BSD License'],
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Version Control',
+                 'Topic :: Utilities'],
     description='vcstool provides a command line tool to invoke vcs commands on multiple repositories.',
     long_description='''\
 vcstool enables batch commands on multiple different vcs repositories. \
 Currently it supports git, hg, svn and bzr.''',
     license='BSD',
-    test_suite='test',
     entry_points={
         'console_scripts': [
             'vcs-branch = vcstool.commands.branch:main',

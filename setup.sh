5aff8feb3028e3705145455b55a25b1a6ec711b9
Dirk Thomas <dthomas@willowgarage.com>
2013-01-14 00:24:01 -0800

    update setup.sh to be source-able from other locations in bash shells

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-export PATH=`pwd`/scripts:$PATH
-export PYTHONPATH=`pwd`:$PYTHONPATH
+if [ "$BASH_SOURCE" ]; then
+  BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+else
+  SCRIPT=$(readlink -f $0)
+  BASEDIR=$(dirname $SCRIPT)
+  if [ ! -f "$BASEDIR/setup.sh" ]; then
+    echo "In non-bash shells the setup.sh file must be sourced from the same directory"
+    return 1
+  fi
+fi
+
+export PATH=$BASEDIR/scripts:$PATH
+export PYTHONPATH=$BASEDIR:$PYTHONPATH

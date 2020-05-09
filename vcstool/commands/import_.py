a2b7d3c6fd6defecd043bf5bab6ce10cf6fbf724
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-05-08 21:03:09 -0700

    clarify help message

--- vcstool/commands/import_.py
+++ vcstool/commands/import_.py
@@ -52,7 +52,7 @@ def get_parser():
     group.add_argument(
         '--skip-existing', action='store_true', default=False,
         help="Don't overwrite existing directories or change custom checkouts "
-             'in repos referencing the same URL')
+             'in repos using the same URL (but fetch repos with same URL')
 
     return parser
 
